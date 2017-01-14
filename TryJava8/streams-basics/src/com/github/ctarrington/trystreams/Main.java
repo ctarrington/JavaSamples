5c6591d204407a94b2dd80bb1cca4a609a076847
CT Arrington <ctarrington@CTs-MacBook-Air.local>
2017-01-14 18:59:35 -0500

    simple streams sample

---
+++ TryJava8/streams-basics/src/com/github/ctarrington/trystreams/Main.java
@@ -0,0 +1,22 @@
+package com.github.ctarrington.trystreams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+
+  public static void main(String[] args) {
+    String raw = "The Quick brown fox jumped over the lazy dog. It was really quick.";
+
+    List<String> finalTokens =
+        Stream.of(raw.split("\\W+"))
+            .map(String::toLowerCase)
+            .distinct()
+            .sorted()
+            .collect(Collectors.toList());
+
+    System.out.println(finalTokens);
+  }
+}
